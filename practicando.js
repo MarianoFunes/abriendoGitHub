a17cf99d1fccd501ad97a8bb06c70de14127096a
MarianoFunes <mgfg83@gmail.com>
2022-09-21 21:46:53 -0300

    vamos de nuevo

---
+++ practicando.js
@@ -0,0 +1,80 @@
+//let url = [ "google.com.ar", "playstation.com.ar"]; 
+
+
+//let valorUrl = url.forEach(function( cantidad, indice){
+
+  //  console.log("Para cada http//www." + cantidad + " existe un valor " + url[indice].length );
+
+//});
+
+//let contarUrl = []
+
+//url.forEach(function (contando){
+
+
+//});
+
+
+let estudiantes = [
+    {nombre: 'John', promedio: 8.5, aprobado: true},
+    {nombre: 'Jane', promedio: 7, aprobado: true},
+    {nombre: 'June', promedio: 3, aprobado: false},
+]
+
+let aprobados = estudiantes.filter(function (aprueban){
+
+ return aprueban.promedio >= 7
+
+});
+
+//console.log(aprobados);
+
+//console.log("estos desaprobaron");
+
+let desaprobados = estudiantes.filter(function ( recuperan ){
+
+    return recuperan.promedio < 7
+    });
+
+    //console.log(desaprobados);
+
+//let desaprobados = estudiantes.filter(function ( recursan ){
+
+//console.log(recursan.promedio <= 7);
+//});
+
+
+// OBJETO DATE
+
+/*let fecha = new Date(); 
+
+let mes = fecha.getMonth();
+let dia = fecha.getDate();
+let anio = fecha.getFullYear();
+
+//console.log("Día: "+fecha.getDate())
+//console.log('Hoy es el día '+ fecha.getDate() +' del mes '+ (fecha.getMonth() + 1) +' del año '+ fecha.getFullYear() );
+
+//DESTRUCTURING 
+
+let arrayPrincipal = [  "Comida", "almuerzo", "cena", "merienda" ]
+
+let [almuerzo, ubfkuebcua, aknuerzo] = arrayPrincipal; 
+
+console.log(arrayPrincipal);
+console.log(almuerzo);
+console.log(ubfkuebcua);*/
+
+
+/*let filtro = [ 1, "calabaza", "retorno", "camin", "torrid" ]
+
+let resultado = filtro.filter(longi => longi.length > 6)
+
+console.log(resultado);
+
+let mapeo = [ 1, 2, 3, 4, 5, 6 ]
+
+let resultadoMapeo = mapeo.map(mapita => mapita)*/
+
+
+
